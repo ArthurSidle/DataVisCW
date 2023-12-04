00f8662479e7ad5b2478fc9ba98745b6c07a9cfc
Arthur <sidle.arthur@gmail.com>
2023-12-04 01:23:41 +0000

    gets data for dash app

---
+++ get_data.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import concurrent
+from dataset_loader import DatasetLoader
+
+
+def get_steam_data():
+    def get_data():
+        dl = DatasetLoader("steam", "steam")
+        steam_df = dl.decompress_data()
+        steam_lost_df = pd.read_csv("data/steam_lost.csv", encoding="unicode_escape")
+
+        steam_lost_df.set_index('AppID', inplace=True)
+
+        return [steam_df.shape[0], steam_lost_df.shape[0]]
+
+    with concurrent.futures.ThreadPoolExecutor(1) as executor:
+        software_counts = executor.submit(get_data).result()
+    
+    data_out = {
+        "percent": {
+            "x": software_counts,
+            "y": ["Available", "Lost"]
+        }
+    }
+
+    return data_out
+    
